d2a386b22f3bac4e3987d17274b1d10b4216894a
kindiwei <kindi.wei@gmail.com>
2016-06-02 02:25:45 -0300

    commit versao 2 beta

---
+++ placascadastradas.php
@@ -0,0 +1,87 @@
+﻿<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+
+<html>
+	<head>
+		<title>Cadastro de veículos autorizados - Bloqueio viário da Prefeitura de Palmital</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<meta http-equiv="content-language" content="pt-br">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+	</head>
+	<body>
+		<div id="site">
+			<div id="cabecalho">
+				<!--<img src="imagens/CabecalhoCART.png">-->
+				<div id="login">
+					<form method="post" action="login.php">
+						<table>
+							<tr><td>CPF:</td><td><input type="text" name="CPFLogin" size=15></td></tr>
+							<tr><td>Senha:</td><td><input type="password" name="senha" size=15></td></tr>
+							<tr><td colspan="2" align="center"><input type="submit" value="Entrar" name="Submit"></td></tr>
+							<tr>
+								<td colspan="2" align="center">
+									<?php
+										// session_start();
+										// if((!isset($_SESSION['login']) == true) and (!isset($_SESSION['senha']) == true) or (strlen($_SESSION['login'])==0) ){
+											echo
+												"<div id='cadastro_esqueci'>
+													<a href='cadastrousuarios.php'>Cadastrar</a>&nbsp;&nbsp;&nbsp;&nbsp;
+													<a href='#'>Esqueci minha senha</a>
+												</div>";
+										// }else{
+											// echo
+												// "<div id='cadastro_esqueci'>
+													// Olá, ".$_SESSION["login"].
+												// "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href='logoff.php'>LOGOUT</a></div>";
+										// }
+									?>
+									<!--<div id="cadastro_esqueci">
+										<a href="cadastrousuarios.php">Cadastrar</a>&nbsp;&nbsp;&nbsp;&nbsp;
+										<a href="#">Esqueci minha senha</a>
+									</div>-->
+								</td>
+							</tr>
+						</table>
+					</form>
+				</div>
+			</div>
+			<div id="menu">
+				<ul>
+					<li><a href="index.php">Cadastro de Placas</a></li>
+					<li><a href="placascadastradas.php">Placas Cadastradas</a></li>
+					<li><a href="cadastrousuarios.php">Cadastro Usuários</a></li>
+				</ul>
+			</div>
+			<div id="conteudo">
+				<b><u><p align="center">CADASTRO DE PLACAS</p></u></b><br><br>
+				<!--<form name="contato" onsubmit="return TestaCampos();" action="envia_email.php" method="post">-->
+					<table cellSpacing="0" cellPadding="0" border="1" align="center">
+						<tr>
+							<th>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Placa&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</th>
+							<th>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Modelo&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</th>
+							<th>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Marca&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</th>
+							<th>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Cor&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</th>
+							<th>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Eixos&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</th>
+						</tr>
+						<tr align="center">
+							<td><b><a href='#' id='placa_teste1'>TTT9999</a></b></td>
+							<td>TXND 4.0</td>
+							<td>Volkswagen</td>
+							<td>Branco</td>
+							<td>5</td>
+						</tr>
+						<tr align="center">
+							<td><b><a href='#' id='placa_teste2'>YYY8888</a></b></td>
+							<td>VLV 5.2</td>
+							<td>Volvo</td>
+							<td>Vermelho</td>
+							<td>7</td>
+						</tr>
+					</table>
+				<!--</form>-->
+			</div>
+			<div id="rodape">
+				<!--<img src="imagens/RodapeCART.png">-->
+			</div>
+		</div>
+	</body>
+</html>
